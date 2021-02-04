342ee517634646c9561360e3b60e89b881656ec5
Comran Morshed <comranmorsh@gmail.com>
2021-02-03 23:35:03 -0800

    Add triangle/sawtooth/square generators

--- src/sources/shape_generators.py
+++ src/sources/shape_generators.py
@@ -5,13 +5,15 @@ from src.dsp.wave import Wave
 from src.util.constants import DEFAULT_SAMPLE_RATE
 
 
-def radian_timeseries(frequency: float, duration_s: float, offset_time_s: float, sample_rate: int) -> np.ndarray:
+def radian_timeseries(
+    frequency: float, duration_s: float, offset_time_s: float, sample_rate: int
+) -> np.ndarray:
     t = np.arange(offset_time_s, duration_s + offset_time_s, 1.0 / sample_rate)
     return 2 * np.pi * frequency * t
 
 
 def generate_sine(
-    frequency: int,
+    frequency: float,
     offset_time_s: float = 0,
     duration_s: float = 1.0,
     sample_rate: int = DEFAULT_SAMPLE_RATE,
@@ -24,7 +26,7 @@ def generate_sine(
 
 
 def generate_square(
-    frequency: int,
+    frequency: float,
     duty_cycle: float = 0.5,
     offset_time_s: float = 0,
     duration_s: float = 1.0,
@@ -37,3 +39,29 @@ def generate_square(
     signal = scipy.signal.square(timeseries)
 
     return Wave(signal, sample_rate)
+
+
+def generate_sawtooth(
+    frequency: float,
+    width: float = 1.0,
+    offset_time_s: float = 0,
+    duration_s: float = 1.0,
+    sample_rate: int = DEFAULT_SAMPLE_RATE,
+) -> Wave:
+
+    width = np.clip(width, 0, 1)
+
+    timeseries = radian_timeseries(frequency, duration_s, offset_time_s, sample_rate)
+    signal = scipy.signal.sawtooth(timeseries, width)
+
+    return Wave(signal, sample_rate)
+
+
+def generate_triangle(
+    frequency: float,
+    offset_time_s: float = 0,
+    duration_s: float = 1.0,
+    sample_rate: int = DEFAULT_SAMPLE_RATE,
+) -> Wave:
+
+    return generate_sawtooth(frequency, 0.5, offset_time_s, duration_s, sample_rate)
--- tst/sources/test_shape_generators.py
+++ tst/sources/test_shape_generators.py
@@ -4,9 +4,12 @@ import pytest
 from src.dsp.fft import Fft
 from src.sources import shape_generators
 
+TEST_FREQUENCIES = [10, 50, 420, 1000, 20000]
+TEST_DURATIONS = [0.1, 0.5, 2]
 
-@pytest.mark.parametrize("frequency", [10, 50, 420, 1000, 20000])
-@pytest.mark.parametrize("duration", [0.1, 0.5, 2])
+
+@pytest.mark.parametrize("frequency", TEST_FREQUENCIES)
+@pytest.mark.parametrize("duration", TEST_DURATIONS)
 def test_generate_sine(frequency: float, duration: float):
     wave = shape_generators.generate_sine(frequency, 0, duration)
     signal = wave.signal
@@ -19,11 +22,37 @@ def test_generate_sine(frequency: float, duration: float):
     np.testing.assert_almost_equal(largest_frequencies[0], frequency, decimal=2)
 
 
-@pytest.mark.parametrize("frequency", [10, 50, 420, 1000, 20000])
-@pytest.mark.parametrize("duration", [0.1, 0.5, 2])
+@pytest.mark.parametrize("frequency", TEST_FREQUENCIES)
+@pytest.mark.parametrize("duration", TEST_DURATIONS)
 def test_generate_square(frequency: float, duration: float):
     wave = shape_generators.generate_square(
         frequency=frequency, offset_time_s=0, duration_s=duration
     )
 
     np.testing.assert_almost_equal(len(wave.signal), wave.sample_rate * duration, decimal=2)
+
+
+@pytest.mark.parametrize("frequency", TEST_FREQUENCIES)
+@pytest.mark.parametrize("duration", TEST_DURATIONS)
+@pytest.mark.parametrize("width", [0, 0.3, 1])
+def test_generate_sawtooth(frequency: float, duration: float, width: float):
+    wave = shape_generators.generate_sawtooth(
+        frequency=frequency, width=width, offset_time_s=0, duration_s=duration
+    )
+
+    np.testing.assert_almost_equal(len(wave.signal), wave.sample_rate * duration, decimal=2)
+
+
+@pytest.mark.parametrize("frequency", TEST_FREQUENCIES)
+@pytest.mark.parametrize("duration", TEST_DURATIONS)
+def test_generate_triangle(frequency: float, duration: float):
+    wave = shape_generators.generate_triangle(
+        frequency=frequency, offset_time_s=0, duration_s=duration
+    )
+
+    sawtooth_wave = shape_generators.generate_sawtooth(
+        frequency=frequency, width=0.5, offset_time_s=0, duration_s=duration
+    )
+
+    np.testing.assert_almost_equal(len(wave.signal), wave.sample_rate * duration, decimal=2)
+    np.testing.assert_array_almost_equal(wave.signal, sawtooth_wave.signal, decimal=2)
